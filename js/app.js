48c7196966d2e30de36645f72ff90a136aae4d82
sebas1197 <eslandazuri@espe.edu.ec>
2023-05-16 11:24:41 -0500

    final

---
+++ js/app.js
@@ -0,0 +1,67 @@
+load_data()
+
+function load_data() {
+    try {
+
+        const table_body = document.querySelector('#table_body')
+
+        fetch('data/vocabulary.json')
+            .then(response => {
+                return response.json()
+            })
+            .then(result => {
+                table_body.innerHTML = ''
+                result.forEach(word => {
+                    table_body.innerHTML += `
+                    <tr>
+                        <td>
+                            ${word.word} &nbsp;&nbsp;
+                            <button class="btn btn-primary btn-sm" onclick="play_audio('${word.word}')">
+                                <i class="bi bi-volume-up"></i>
+                            </button>
+                        </td>
+                        <td>${word.translate}</td>
+                    </tr>
+                `
+                })
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function play_audio(word) {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = word;
+    utterance.lang = 'en-US'; // Establece el idioma en inglés (en-US)
+
+    // Reproduce la pronunciación
+    speechSynthesis.speak(utterance);
+}
+
+function play_audio_es(word) {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = word;
+    utterance.lang = 'es-ES'; // Establece el idioma en inglés (en-US)
+
+    // Reproduce la pronunciación
+    speechSynthesis.speak(utterance);
+}
+
+function play_all() {
+    try {
+
+        fetch('data/vocabulary.json')
+            .then(response => {
+                return response.json()
+            })
+            .then(result => {
+                result.forEach(word => {
+                    play_audio(word.word)
+                    play_audio_es(word.translate)
+                })
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
